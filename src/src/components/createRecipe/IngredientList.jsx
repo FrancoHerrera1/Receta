8d10097197d065755cfa1f77d64b258cd8294530
¨FrancoHerrera1¨ <francocalf@gmail.com>
2025-01-07 00:10:31 -0300

    Cambios

---
+++ src/src/components/createRecipe/IngredientList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const IngredientList = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default IngredientList;
---
+++ src/src/components/createRecipe/RecipeBuilder.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./recipeBuilder.css";
+
+export const RecipeBuilder = () => {
+  return (
+    <div></div>
+  );
+};
+
+
+export default RecipeBuilder;
